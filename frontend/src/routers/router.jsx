99be277f62a43c9279f6e4c2343957dbd4d92c4c
Henry Jonathan <henryjonathanchandra12@gmail.com>
2025-01-04 23:36:50 +0700

    add additional folders in frontend

---
+++ frontend/src/routers/router.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom"
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <div>Tes</div>
+    }
+])
+
+export default router
