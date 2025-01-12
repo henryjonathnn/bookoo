92ccf748e8671610cabe1c05a918cbb44642fd06
Henry Jonathan <henryjonathanchandra12@gmail.com>
2025-01-12 14:47:59 +0700

    build the route for users and use in main/index file

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,14 @@
 import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
+import UserRoute from "./routes/UserRoute.js"
 dotenv.config()
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(UserRoute)
 
 app.listen(process.env.APP_PORT, () => {
     console.log('Server berhasil running..')
