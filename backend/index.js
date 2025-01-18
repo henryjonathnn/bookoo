e5fd606f8ac3bcbe27a974fcc8532d7dfde13b15
Henry Jonathan <henryjonathanchandra12@gmail.com>
2025-01-18 14:24:21 +0700

    update book endpoints to handle upload image

--- backend/index.js
+++ backend/index.js
@@ -20,6 +20,7 @@ const app = express();
 app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(express.json());
 app.use(cookieParser());
+app.use(express.static("public"));
 
 app.use(router);
 
--- backend/routes/BukuRoutes.js
+++ backend/routes/BukuRoutes.js
@@ -1,12 +1,13 @@
 import express from "express"
 import { createBuku, deleteBuku, getBuku, getBukuById, updateBuku } from "../controllers/BukuController.js"
+import upload from "../middlewares/uploadImage.js"
 
 const router = express.Router()
 
 router.get("/", getBuku)
 router.get("/:id", getBukuById)
-router.post("/", createBuku)
-router.patch("/:id", updateBuku)
+router.post("/", upload.single("cover_img"), createBuku)
+router.patch("/:id", upload.single("cover_img"), updateBuku)
 router.delete("/:id", deleteBuku)
 
 export default router
